92e03e91073af76696486f6e4423d6687e680a82
Joon Hur <hurjun1995@gmail.com>
2019-10-20 21:46:04 -0700

    created shell that extends cmd to make the program interactive

--- main.py
+++ main.py
@@ -1,18 +1,29 @@
 #! ~/Env/holololo python
 
+import cmd
 import requests
 from bs4 import BeautifulSoup
 
-def main():
-    champion = 'ahri'
-    r = requests.get("https://na.op.gg/champion/{champ}/statistics/mid".format(champ=champion))
+
+class HololoShell(cmd.Cmd):
+    intro = 'Welcome to the league of legend shell. Type help or ? to list commands.\n'
+    prompt = '> '
+
+    def do_skill(self, arg):
+        print_skill(arg)
+
+
+def print_skill(champ_name):
+    r = requests.get("https://na.op.gg/champion/{champ}/statistics/mid".format(champ=champ_name))
     soup = BeautifulSoup(r.text, features="html.parser").find('body')
     spell_and_skill = soup.find_all('table')[2]
     skills = spell_and_skill.find_all('tbody')[1]
-
     skill_master_order = [skill.text for i, skill in enumerate(skills.find_all('span')) if i < 3]
-
     print("skill master order: ", ' -> '.join(skill_master_order))
+
+
+def main():
+    HololoShell().cmdloop()
 
 
 if __name__ == "__main__":
