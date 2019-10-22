1752cb645e859b92c0fcab76fb6f114f376e0898
Joon Hur <hurjun1995@gmail.com>
2019-10-22 00:01:50 -0700

    redeisnged fetch to request from specific path such as: match, skill, item * redesigned print_skill to print 2 possible skill orders with win_rate and pick_rate * done print_counter

--- main.py
+++ main.py
@@ -1,12 +1,25 @@
-#! ~/Env/holololo python
+#! ~/.virtualenv/leaguekipedia python
 
 import argparse, json, requests
 from bs4 import BeautifulSoup
+from enum import Enum
+
+
+class Lane(Enum):
+    top = 'top'
+    jg = 'jungle'
+    mid = 'mid'
+    bot = 'bot'
+    sup = 'support'
+
+    def __str__(self):
+        return self.name
 
 
 class LeagueKipedia:
-    soup = None
     champion_info_dict = None
+    champ_name = None
+    champ_lane = None
 
     def __init__(self):
         self.champion_info_dict = self.get_official_champion_info()
@@ -18,37 +31,70 @@ class LeagueKipedia:
         champion_info_dict = json_data["data"]
         return champion_info_dict
 
-    def is_valid_champion(self, champ_name):
+    def is_valid_champion(self):
         """check if given champion name exists in riot's official champion list"""
-        champ_name = champ_name.lower()
+        champ_name = self.champ_name.lower()
         s = list(champ_name)
         s[0] = s[0].upper()
         champ_name = "".join(s)
         return champ_name in self.champion_info_dict
 
     def print_skill(self):
-        spell_and_skill = self.soup.find_all('table')[2]
-        skills = spell_and_skill.find_all('tbody')[1]
-        skill_master_order = [skill.text for i, skill in enumerate(skills.find_all('span')) if i < 3]
-        print("skill master order: ", ' -> '.join(skill_master_order))
+        # fetch and parse information
+        res = requests.get("https://na.op.gg/champion/{champ}/statistics/{lane}/skill?"
+                           .format(champ=self.champ_name, lane=self.champ_lane))
+        soup = BeautifulSoup(res.content, features="html.parser")
+        skill_order_info_list = soup.find("div", class_="tabItem Content championLayout-skill") \
+            .find('div', class_='champion-box-content') \
+            .find_all('li', class_='champion-stats__filter__item')
+        skill_order_info_list = skill_order_info_list[0:2]
 
-    @staticmethod
-    def print_counter():
-        print("print counter: stub")
+        # print
+        print("Skill Orders:")
+        print("{:>12} {:>20} {:>12} {:>12}".format("index", "skill order", "win rate", "pick rate"))
+        for i, skill_order_info in enumerate(skill_order_info_list):
+            skill_order = ' -> '.join(
+                [s.find('span').text for s in skill_order_info.find_all('li', class_='champion-stats__list__item')])
+            # somehow class names are reversed for pick_rate and win_rate on op.gg
+            win_rate = skill_order_info\
+                .find('div', class_='champion-stats__filter_item_value--pickrate')\
+                .find('b').text
+            pick_rate = skill_order_info\
+                .find('div', class_='champion-stats__filter_item_value--winrate')\
+                .find('b').text
+            print("{:>12} {:>20} {:>12} {:>12}".format(i, skill_order, win_rate, pick_rate))
+
+    def print_counter(self):
+        # fetch and parse information
+        res = requests.get("https://na.op.gg/champion/{champ}/statistics/{lane}/matchup?"
+                           .format(champ=self.champ_name, lane=self.champ_lane))
+        soup = BeautifulSoup(res.content, features="html.parser")
+        counter_champion_list = soup.find_all("div", class_="champion-matchup-champion-list__item")
+        counter_champion_list \
+            .sort(key=lambda c: float(c.find('span', class_="champion-matchup-list__winrate").text.strip().strip('%')))
+
+        # print
+        print("Counter Champions:")
+        print("{:>12} {:>13} {:>14} {:>15}".format("", "name", "win rate", "total played"))
+        for i, champ in enumerate(counter_champion_list):
+            name = champ.find('div', class_='champion-matchup-list__champion').find('span').text
+            win_rate = champ.find('span', class_='champion-matchup-list__winrate').text.strip()
+            total_played = champ.find('div', class_='champion-matchup-list__totalplayed').find('span').text
+            print("{:>12} {:>13} {:>14} {:>15}".format(i, name, win_rate, total_played))
 
     @staticmethod
     def print_item():
         print("print item: stub")
 
     def handle_champion_command(self, args):
-        champ_name = args.champion_name
-        if not self.is_valid_champion(champ_name):
-            print("'{champ}' is not existing champion name".format(champ=champ_name))
+        self.champ_name = args.champion_name
+        self.champ_lane = args.lane.value
+
+        if not self.is_valid_champion():
+            print("'{champ}' is not existing champion name".format(champ=self.champ_name))
             return
 
-        res = requests.get("https://na.op.gg/champion/{champ}/statistics/".format(champ=champ_name))
-        self.soup = BeautifulSoup(res.content, features="html.parser").find('body')
-
+        print("information on: {champ} at {lane}".format(champ=self.champ_name, lane=self.champ_lane))
         if not args.skill and not args.counter and not args.item:
             self.print_skill()
             self.print_item()
@@ -56,24 +102,29 @@ class LeagueKipedia:
         else:
             if args.skill:
                 self.print_skill()
-            if args.counter:
-                self.print_counter()
             if args.item:
+                self.print_item()
+            if args.counter:
                 self.print_counter()
 
     def main(self):
-        command_parser = argparse.ArgumentParser("Leagkipedia: League of Legend cheat sheet")
+        command_parser = argparse.ArgumentParser()
         subparsers = command_parser.add_subparsers()
 
         champion_subparser = subparsers.add_parser("champion")
         champion_subparser.add_argument('champion_name', type=str, help="Name of the champion")
-        champion_subparser.add_argument('-s', '--skill', action='store_true')
-        champion_subparser.add_argument('-c', '--counter', action='store_true')
-        champion_subparser.add_argument('-i', '--item', action='store_true')
+        champion_subparser.add_argument('lane', type=Lane, choices=list(Lane), help='Lane to which champion goes')
+        champion_subparser.add_argument('-s', '--skill', action='store_true',
+                                        help='include to see skill master order in the output')
+        champion_subparser.add_argument('-c', '--counter', action='store_true',
+                                        help='include to see counter champions in the output')
+        champion_subparser.add_argument('-i', '--item', action='store_true',
+                                        help='include to see item trees in the output')
         champion_subparser.set_defaults(func=self.handle_champion_command)
 
         args = command_parser.parse_args()
         args.func(args)
+
 
 if __name__ == "__main__":
     LeagueKipedia().main()
