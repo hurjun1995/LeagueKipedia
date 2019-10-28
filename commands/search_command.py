6734740ef5564e58b41ccdbe404dd401b870e92e
Joon Hur <hurjun1995@gmail.com>
2019-10-28 08:36:47 -0700

    multiple search logic done

--- commands/search_command.py
+++ commands/search_command.py
@@ -1,10 +1,12 @@
+import pyperclip
+
 from commands.command_base import CommandBase
 from sub_commands.champions_stat_sub_command import ChampionsStatSubCommand
 
 
 class SearchCommand(CommandBase):
     champions_stat_sub_command = None
-
+    
     def __init__(self):
         self.champions_stat_sub_command = ChampionsStatSubCommand()
 
@@ -14,7 +16,20 @@ class SearchCommand(CommandBase):
         search_subparser.set_defaults(func=self.run_command)
 
     def run_command(self, args):
-        self.champions_stat_sub_command.set_user_name(' '.join(args.username))
+        if args.username is not None:
+            self.run_champions_stat_sub_command(' '.join(args.username))
+        else:
+            clipboard_text = pyperclip.paste()
+            if clipboard_text is None:
+                return
+            else:
+                user_id_set = {line.split()[0] for line in clipboard_text.split('\n')}
+                for user_id in user_id_set:
+                    print(user_id)
+                    self.run_champions_stat_sub_command(user_id)
+
+    def run_champions_stat_sub_command(user_name):
+        self.champions_stat_sub_command.set_user_name(id)
         self.champions_stat_sub_command.retrieve()
         self.champions_stat_sub_command.parse()
         self.champions_stat_sub_command.print()
